3d5dad5f495ae8a6ed16e80529ae66554b415d50
Piyasa Bera <105113568+Piyasa17@users.noreply.github.com>
2022-06-15 10:03:12 +0530

    Create 48.cpp

---
+++ 48.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j++){
+                if(i!=j){
+                    int temp=matrix[i][j];
+                    matrix[i][j]=matrix[j][i];
+                    matrix[j][i]=temp;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
+    }
+};
